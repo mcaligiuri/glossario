26940e027a8034348f83e9211945fce44a30c239
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2018-12-27 02:27:25 +0100

    Aggiunti pulsanti di navigazione e titolo per il div delle materie

--- header.php
+++ header.php
@@ -1,5 +1,12 @@
 <header>
     <h1>Glossario<a class="fa fa-bars" id="mnu" onclick="apri()"></a></h1>
+    <!-- Menù di navigazione -->
+    <nav>
+    <a href="./" class="fa fa-home" style="font-size: 18px;"><span> Home</span></a>
+    <a href="index.php" class="fa fa-info-circle" style="font-size: 18px;"><span> Info</span></a>
+    <a href="./amministra" class="fa fa-key" style="font-size: 18px;"><span> Admin</span></a>
+    </nav>
+    <!-- Materie -->
     <div id="mat" class="matside">
         <a href="javascript:void(0)" class="btnchiudi" onclick="chiudi()">&times;</a>
         <?php
@@ -7,7 +14,8 @@
         $righe = $dbconn->query($sql);
         if($righe->num_rows == 0)
             die("Nessuna materia, aggiungine una");
-
+        
+        echo "<h4>Materie</h4>\n";
         while($riga = $righe->fetch_assoc())
         {
             $idm = $riga['idm'];
