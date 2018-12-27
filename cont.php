205db3d30cd27f4fe75a0566e160f10f069b3909
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2018-12-27 02:25:41 +0100

    aggiunto messaggio di benvenuto

--- cont.php
+++ cont.php
@@ -8,6 +8,7 @@
 
 <!-- Descrizione iniziale o significato di un termine -->
 <article id="cont">
+    <h4>Benvenuti!</h4>
     <p>Un glossario è una raccolta di termini appartenenti ad un ambito specifico e circoscritto. 
     In alcuni casi il termine glossario è utilizzato per indicare la raccolta e relativa spiegazione 
     dei termini desueti o specialistici utilizzati in un libro (generalmente collocato alla fine del testo).</p>
