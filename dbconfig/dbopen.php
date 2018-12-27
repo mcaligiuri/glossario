66038da77f5fc2b7dda7c8ab88cf921dbcc57b2e
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2018-12-27 02:27:55 +0100

    Aggiustamenti

--- dbconfig/dbopen.php
+++ dbconfig/dbopen.php
@@ -5,7 +5,7 @@ $dbconn = new mysqli($dbhost,$dbuser,$dbpass, $dbname);
 
 // Controllo se la connessione è andata a buon fine
 if($dbconn->connect_error)
-    die("Errore Mysql: ". $dbconn->connect_errno . "-" . $dbconn->connect_error);
+    die("Errore Mysql: N°". $dbconn->connect_errno . "-" . $dbconn->connect_error);
 
 $dbconn->set_charset("UTF8");
 ?>
