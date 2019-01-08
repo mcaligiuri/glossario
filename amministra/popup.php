59a8e2153d7ab41ece9e251f1d97ef17adc93dd5
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2019-01-09 00:30:35 +0100

    Popup di conferma per le operazioni di aggiornamento

---
+++ amministra/popup.php
@@ -0,0 +1,8 @@
+<div class='popup' id='popup'>
+    <div id="pophead"><h3>Avviso</h3></div>
+    <div id='popcont'><p>Vuoi salvare le modifiche?</p></div>
+    <div id="popcom">
+        <button id='idyessa'>Si</button>
+        <button id='idno'>No</button>
+    </div>
+</div>
