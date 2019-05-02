848eff5f795ddafc09c9c6368aa384111a7614f7
Michele Caligiuri <miche@DESKTOP-DKIQGSN>
2019-05-02 10:49:55 +0200

    Inserisce termini

--- amministra/insert/insTerm.php
+++ amministra/insert/insTerm.php
@@ -1,11 +1,12 @@
 <?php
-if(!isset($_GET['term']) || !isset($_GET['coda']) || !isset($_GET['def']))
+if(!isset($_GET['term']) || !isset($_GET['coda']) || !isset($_GET['def'])) // Controllo
     header('Location:../');
 
 $term = addslashes($_GET['term']);
 $def = addslashes($_GET['def']);
 $coda = $_GET['coda'];
 include "../../dbconfig/dbopen.php";
+// Inserisco un nuovo termine con definizione nel database
 $sql="INSERT INTO $ter (coda,termine,definizione) VALUES ('$coda','$term','$def')";
 if (!$dbconn->query($sql))
 {
