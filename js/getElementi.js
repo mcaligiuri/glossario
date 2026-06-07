9169a5b366b5db7ff7ade2e74e0b89d562490212
Michele <michele.caligiuri@paoloastori.com>
2026-06-07 16:17:01 +0200

    sintassi più moderna, uso di fetch

--- js/getElementi.js
+++ js/getElementi.js
@@ -1,95 +1,125 @@
 // Tecnica Ajax usata per aggiornare i contenuti in background
 
 // Richiedo argomenti in background alla pagina PHP
-function getArgomenti(codm,mat)
-{
+async function getArgomenti(codm,mat) {
   // Variabili
-  var cont, termini,argomenti;
-  termini = document.getElementById("term");
-  argomenti = document.getElementById("arg");
-  cont = document.getElementById("cont");
+  const termini   = document.getElementById("term");
+  const argomenti = document.getElementById("arg");
+  const cont      = document.getElementById("cont");
 
   // Chiudo barra di navigazione
   chiudi();
   
   // Gestisco richiesta
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() 
-  {
-    if (this.readyState == 4 && this.status == 200) 
-    {
+  try {
+    const par = new URLSearchParams();
+    par.append("codm",codm);
+    par.append("mat",mat);
+    const ric = await fetch("arg.php?"+ par.toString(), {
+      method: "GET",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (ric.ok) {
       argomenti.style.display = "block";
       cont.style.display = "none";
-      argomenti.innerHTML = this.responseText;
-      if(termini.style.display=="block" || cont.style.display=="block")
-      {
-        termini.style.display="none";
-        cont.style.display="none";
+      argomenti.innerHTML = await ric.text();
+      if(termini.style.display === "block" || cont.style.display === "block") {
+        termini.style.display = "none";
+        cont.style.display = "none";
       }
     }
-  };
-  xmlhttp.open("GET", "arg.php?codm="+ codm+"&mat="+mat, true);
-  xmlhttp.send();
+    else {
+      console.error("Errore nel recupero degli argomenti.");
+    }
+  }
+  catch(e) {
+    console.error("Si è verificato un problema nell'aggiornare la pagina", e);   
+  }
 }
-
+ 
 // Richiedo termini in background alla pagina PHP
-function getTermini(coda,mat,arg)
-{
-  var termini, argomenti;
-  termini = document.getElementById("term");
-  argomenti = document.getElementById("arg");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() 
-  {
-    if (this.readyState == 4 && this.status == 200) 
-    {
+async function getTermini(coda,mat,arg) {
+  const termini = document.getElementById("term");
+  const argomenti = document.getElementById("arg");
+  try {
+    const par = new URLSearchParams();
+    par.append("coda",coda);
+    par.append("arg",arg);
+    par.append("mat",mat);
+    const ric = await fetch("term.php?" + par.toString(), {
+      method: "GET",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (ric.ok) {
       argomenti.style.display = "none";
       termini.style.display = "block";
-      termini.innerHTML = this.responseText;
+      termini.innerHTML = await ric.text();
     }
-  };
-  xmlhttp.open("GET", "term.php?coda="+ coda + "&arg="+arg +"&mat="+mat  , true);
-  xmlhttp.send();
+    else {
+      console.error("Errore nel recupero dei termini.");
+    }
+  }
+  catch(e) {
+    console.error("Si è verificato un problema", e);   
+  }
 }
 
 // Richiedo definizioni in background alla pagina PHP
-function getDef(idt)
-{
-  var cont;
-  termini = document.getElementById("term");
-  cont = document.getElementById("cont");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() 
-  {
-    if (this.readyState == 4 && this.status == 200)
-    {
+async function getDef(idt) {
+  //var cont;
+  const termini = document.getElementById("term");
+  const cont = document.getElementById("cont");
+  try {
+    const par = new URLSearchParams();
+    par.append("idt",idt);
+    const ric = await fetch("def.php?"+ par.toString(), {
+      method: "GET",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (ric.ok) {
       cont.style.display = "block";
       termini.style.display = "none";
-      cont.innerHTML = this.responseText;
+      cont.innerHTML = await ric.text();
     }
-  };
-  xmlhttp.open("GET", "def.php?idt="+ idt, true);
-  xmlhttp.send();
+    else {
+      console.error("Errore nel recupero delle definizioni.");
+    }
+  }
+  catch(e) {
+    console.error("Si è verificato un problema", e);
+  }
 }
 
 // Pagina delle info
-function getInfo(page)
-{
-  var cont,arg,term;
-  term = document.getElementById("term");
-  arg = document.getElementById("arg");
-  cont = document.getElementById("cont");
-  var xmlhttp = new XMLHttpRequest();
-  xmlhttp.onreadystatechange = function() 
-  {
-    if (this.readyState == 4 && this.status == 200)
-    {
+async function getInfo(page) {
+  //var cont,arg,term;
+  const term = document.getElementById("term");
+  const arg = document.getElementById("arg");
+  const cont = document.getElementById("cont");
+  try {
+    const ric = await fetch(page, {
+      method: "GET",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded",
+      },
+    });
+    if (ric.ok) {
       cont.style.display = "block";
       arg.style.display = "none";
       term.style.display = "none";
-      cont.innerHTML = this.responseText;
+      cont.innerHTML = await ric.text();
     }
-  };
-  xmlhttp.open("GET",page, true);
-  xmlhttp.send();
+    else {
+      console.error("Errore nel caricamento delle info");
+    }
+  }
+  catch(e) {
+    console.error("Si è verificato un problema", e);
+  }
 }
