fa1c0c0e51a523586e679a402fe4825ef8bd5fab
Michele <michele.caligiuri@paoloastori.com>
2026-06-07 23:07:27 +0200

    Disattivo autocompletamento browser perchè brutto da vedere

--- amministra/login/form.php
+++ amministra/login/form.php
@@ -1,10 +1,15 @@
+<?php
+session_start();
+$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+?>
 <!-- Struttura della login -->
 <div class="form" id="form">
-    <form onsubmit="setLogin(); return false;">
-        <p id="msg"></p>
-        <input type="text" placeholder="Username" title="username" id="user" required>
-        <input type="password" placeholder="Password" title="password" id="pass" required>
-        <input type="submit" name="accedi" title="accedi" value="Accedi">
-        <input type="reset" name="annulla" title="annulla" value="Annulla" onclick="resetForm();">
-    </form>
+  <form autocomplete="off" onsubmit="setLogin(); return false;">
+    <p id="msg"></p>
+    <input type="text" placeholder="Username" title="username" id="user" required>
+    <input type="password" placeholder="Password" title="password" id="pass" required>
+    <input type="submit" name="accedi" title="accedi" value="Accedi">
+    <input type="reset" name="annulla" title="annulla" value="Annulla" onclick="resetForm();">
+    <input type="text" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>" hidden>
+  </form>
 </div>
