ef50786e53da055f997162e5ad85ee33ba7fc432
Michele <michele.caligiuri@paoloastori.com>
2026-06-07 16:16:11 +0200

    Dichiarazione e definzione delle variabili più moderna

--- js/filtraListe.js
+++ js/filtraListe.js
@@ -1,50 +1,37 @@
 // Funzioni per eseguire una ricerca rapida nelle varie liste
-function getElem(val) 
-{
-    var term,cont;
-    cont = document.getElementById("cont");
-    term = document.getElementById("term");
-    switch(val)
-    {
+function getElem(val) {
+  const cont = document.getElementById("cont");
+  const term = document.getElementById("term");
+  const input = document.getElementById("cerca");
+  const filter = input.value.toUpperCase();
+  const div = document.getElementById("arg");
+  const a = div.getElementsByTagName("a"); 
+  let txtValue = "";
+  switch(val) {
     // Filtro argomenti
-    case 1:
-        if(term.style.display == "block" || cont.style.display == "block")
-        {
-            term.style.display = "none";
-            cont.style.display = "none";
-        }
-        var input, filter, a, i;
-        input = document.getElementById("cerca");
-        filter = input.value.toUpperCase();
-        div = document.getElementById("arg");
-        a = div.getElementsByTagName("a");
-        for (i = 0; i < a.length; i++) 
-        {
-            txtValue = a[i].textContent || a[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) 
-                a[i].style.display = "";
-            else 
-                a[i].style.display = "none";
-        }
-        break;
-    // Filtro termini
-    case 2:
-        if(cont.style.display == "block")
-            cont.style.display = "none";
-    
-        var input, filter, a, i;
-        input = document.getElementById("cercat");
-        filter = input.value.toUpperCase();
-        div = document.getElementById("term");
-        a = div.getElementsByTagName("a");
-        for (i = 0; i < a.length; i++) 
-        {
-            txtValue = a[i].textContent || a[i].innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) 
-                a[i].style.display = "";
-            else 
-                a[i].style.display = "none";
+    case 1: {
+      if(term.style.display === "block" || cont.style.display === "block") {
+        term.style.display = "none";
+        cont.style.display = "none";
+      }
+      for (let i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) a[i].style.display = "";
+        else a[i].style.display = "none";
         }
         break;
     }
+    // Filtro termini
+    case 2: {
+      if(cont.style.display == "block")
+        cont.style.display = "none";
+  
+      for (let i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) a[i].style.display = "";
+        else a[i].style.display = "none";
+      }
+      break;
+    }
+  }
 }
