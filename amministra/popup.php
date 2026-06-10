6c53da8a2c5b0539e4d0f59410a753e0d9151cee
Michele <michele.caligiuri@paoloastori.com>
2026-06-10 23:41:35 +0200

    aggiunti id di gestione

--- amministra/popup.php
+++ amministra/popup.php
@@ -1,9 +1,9 @@
 <!-- Popup di conferma a ogni operazione -->
 <div class='popup' id='popup'>
-    <div id="pophead"><h3>Avviso</h3></div>
-    <div id='popcont'><p>Vuoi salvare le modifiche?</p></div>
-    <div id="popcom">
-        <button id='idyessa'>Si</button>
-        <button id='idno'>No</button>
-    </div>
+  <div id="pophead"><h3 id='poptitolo' >Avviso</h3></div>
+  <div id='popcont'><p id='poptesto'>Vuoi procedere e salvare le modifiche?</p></div>
+  <div id="popcom">
+    <button id='idyessa'>Si</button>
+    <button id='idno'>No</button>
+  </div>
 </div>
