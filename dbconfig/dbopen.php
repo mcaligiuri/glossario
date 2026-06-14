160719a37c50cbced81936679fc2c9973caa2f57
Michele <michele.caligiuri@paoloastori.com>
2026-06-14 19:02:31 +0200

    codice di connessione più moderno e preciso con decodifica degli errori in messaggi umanamente comprensibili

--- dbconfig/dbopen.php
+++ dbconfig/dbopen.php
@@ -1,11 +1,28 @@
 <?php
 // Dati di connessione
 include "dbconfig.php";
-$dbconn = new mysqli($dbhost,$dbuser,$dbpass, $dbname);
 
-// Controllo se la connessione è andata a buon fine
-if($dbconn->connect_error)
-  die("Errore MariaDB: N°". $dbconn->connect_errno . "-" . $dbconn->connect_error);
-
-$dbconn->set_charset("utf8mb4"); // Codifica
+try {
+  $dbconn = new mysqli($dbhost,$dbuser,$dbpass, $dbname);
+  $dbconn->set_charset("utf8mb4"); // Codifica
+}
+catch(mysqli_sql_exception $e) {
+  // In base al codice di errore di MariaDB visualizzo errore comprensibile
+  switch ($e->getCode()) {
+    case 1045:
+      $messaggio = "Accesso negato! Controlla username e password.";
+      break;
+    case 1049:
+      $messaggio = "Il database selezionato non esiste.";
+      break;
+    case 2002:
+      $messaggio = "Il server del database è offline o l'host è errato.";
+      break;
+    default:
+      $messaggio = "Si è verificato un problema di connessione imprevisto.";
+      break;
+  }
+  die("<h1 id='dberror'><strong>Errore di connessione MariaDB: ". $messaggio."</strong></h1>");
+  
+}
 ?>
