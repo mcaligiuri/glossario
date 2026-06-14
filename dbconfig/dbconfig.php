6ebec40a4ad24657c9947744b43a5cb0e03383ed
Michele <michele.caligiuri@paoloastori.com>
2026-06-14 19:01:45 +0200

    credenziali didattiche specifiche per il progetto

--- dbconfig/dbconfig.php
+++ dbconfig/dbconfig.php
@@ -1,9 +1,12 @@
 <?php
 // Dati di connessione
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "my_michelecaligiuri";
+$dbhost = "127.0.0.1";
+$dbuser = "glossario";
+$dbpass = "glossario";
+$dbname = "glossario";
+
+// Visualizzazione errori database
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 // Tabelle del database
 include "global.php";
