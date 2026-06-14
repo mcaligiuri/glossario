266aa13d6fc10844e5792cb10fefb4f65fe97882
Michele <michele.caligiuri@paoloastori.com>
2026-06-14 16:42:20 +0200

    abbellimenti

--- amministra/cont.php
+++ amministra/cont.php
@@ -1,16 +1,16 @@
 <article class="cont">
-    <!-- Materie -->
-    <div id="mat">
-        <?php 
-        echo "<h3>Ciao ".ucfirst($_SESSION['user']).
-        " !</h3>\n<p>Da qui puoi gestire le materie scolastiche. Hai la possibilità di aggiungere, modificare e rimuovere argomenti e termini.</p>\n";
-        ?>
-    </div>
-    <!-- Argomenti -->
-    <div id="arg"></div>
-    <!-- Termini -->
-    <div id="ter"></div>
-    <!-- Definizione -->
-    <div id="def"></div>
-    <!-- <div id="use"></div> -->
+  <!-- Materie -->
+  <div id="mat">
+    <?php 
+    echo "<h3 style='border-bottom: 2px solid #ccc;padding-bottom: 5px;'>Ciao ".ucfirst($_SESSION['user']).
+    " ! Iniziamo a studiare.</h3>\n<p>Da qui puoi gestire le materie scolastiche. Hai la possibilità di aggiungere, modificare e rimuovere argomenti e termini.</p>\n";
+    ?>
+  </div>
+  <!-- Argomenti -->
+  <div id="arg"></div>
+  <!-- Termini -->
+  <div id="ter"></div>
+  <!-- Definizione -->
+  <div id="def"></div>
+  <!-- <div id="use"></div> -->
 </article>
