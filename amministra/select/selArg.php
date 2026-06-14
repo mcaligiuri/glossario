70ca2ee94ecac7db20e72f5d2c5b989e77d98d14
Michele <michele.caligiuri@paoloastori.com>
2026-06-14 17:43:41 +0200

    abbellimenti, rimossa addslashes

--- amministra/select/selArg.php
+++ amministra/select/selArg.php
@@ -8,12 +8,12 @@
 
 include "../../dbconfig/dbopen.php";
 $codm = $_POST['codm'];
-$nome = addslashes($_POST['mat']);
+$nome = htmlspecialchars($_POST['mat']);
 $sql = "SELECT * FROM $arg WHERE codm=$codm ORDER BY argomento";
 $righe = $dbconn->query($sql);
 echo "<table>\n";
 echo "<tr><td id='thead'><h3>Argomenti di $nome</h3></td>\n";
-echo "<td id='thead'><a id='ind' onclick='getMaterie();'>indietro</a></td></tr></table>\n";
+echo "<td id='thead'><a href='#' id='ind' onclick='getMaterie();'>indietro</a></td></tr></table>\n";
 if($righe->num_rows == 0) {
   echo "Nessun argomento da mostrare";
   echo "<input type='text' id='insarg' placeholder='aggiungi'><a id='btnins' class='fa fa-plus' onclick=\"insArg($codm,'$nome')\"></a>";
@@ -25,13 +25,13 @@ echo "<table>\n";
 echo "<tr><th>Argomento</th>\n";
 echo "<th>Modifica</th>\n";
 echo "<th>Elimina</th></tr>\n";
-$materia = $_POST['mat'];
+$materia = htmlspecialchars($_POST['mat']);
 while($riga = $righe->fetch_assoc()) {
   $ida = $riga['ida'];
   $nome = $riga['argomento'];
-  echo "<tr><td><a id='lnkarg' onclick=\"getTerm('$ida','$nome');\">".$riga['argomento']."</a></td>\n";
-  echo "<td><a onclick=\"getForm('$ida','arg','$nome','$codm');\" class='fa fa-pencil' id='btnmod'></a></td>\n";
-  echo "<td><a id='btndel' onclick=\"delArg('$ida','$codm','$materia');\" class='fa fa-window-minimize'></a></td></tr>\n";
+  echo "<tr><td><a href='#' title='vedi termini per questo argomento' id='lnkarg' onclick=\"getTerm('$ida','$nome');\">".$riga['argomento']."</a></td>\n";
+  echo "<td><a href='#' title='rinomina' onclick=\"getForm('$ida','arg','$nome','$codm');\" class='fa fa-pencil' id='btnmod'></a></td>\n";
+  echo "<td><a href='#' id='btndel' title='elimina' onclick=\"delArg('$ida','$codm','$materia');\" class='fa fa-window-minimize'></a></td></tr>\n";
 }
 echo "</table>\n";
 include "../../dbconfig/dbclose.php";
--- amministra/select/selTerm.php
+++ amministra/select/selTerm.php
@@ -1,6 +1,8 @@
 <?php
- if(!isset($_GET['coda']) || !isset($_GET['arg']))
-     header('Location:../');
+ if(!isset($_GET['coda']) || !isset($_GET['arg'])) {
+   header('Location:../');
+   exit;
+ }
 
 include "../../dbconfig/dbopen.php";
 $coda = $_GET['coda'];
@@ -12,21 +14,19 @@ echo "<tr><td id='thead'><h3>Termini di $nome</h3></td>\n";
 echo "<td id='thead'><a id='ind' onclick='showArg();'>indietro</a></td></tr></table>\n";
 echo "<input type='text' id='insterm' placeholder='Nuovo termine'><textarea id='insdef' placeholder='Definizione'></textarea>\n";
 echo "<a id='btnins' class='fa fa-plus' onclick=\"insTerm('$coda','$nome')\"></a>\n";
-if($righe->num_rows == 0)
-{
-    echo "<p>Nessun termine da mostrare</p>\n";
-    include "../../dbconfig/dbclose.php";
-    return;
+if($righe->num_rows == 0) {
+  echo "<p>Nessun termine da mostrare</p>\n";
+  include "../../dbconfig/dbclose.php";
+  return;
 }
 echo "<table>\n";
 echo "<th>Termine</th>\n";
 echo "<th>Elimina</th>\n";
-while($riga = $righe->fetch_assoc())
-{
-    $idt = $riga['idt'];
-    $term = $riga['termine'];
-    echo "<tr><td><a id='lnkter' onclick=\"getDef($idt,$coda,'$nome');\">$term</a></td>\n";
-    echo "<td><a id='btndel' onclick=\"delTerm($idt,$coda,'$nome');\" class='fa fa-window-minimize'></a></td><tr>\n";
+while($riga = $righe->fetch_assoc()) {
+  $idt = $riga['idt'];
+  $term = $riga['termine'];
+  echo "<tr><td><a id='lnkter' onclick=\"getDef($idt,$coda,'$nome');\">$term</a></td>\n";
+  echo "<td><a id='btndel' onclick=\"delTerm($idt,$coda,'$nome');\" class='fa fa-window-minimize'></a></td><tr>\n";
 }
 echo "<table></table>";
 include "../../dbconfig/dbclose.php";
