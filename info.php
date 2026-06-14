b29ba27568cd8bba46d14bdae351c95960d495a9
Michele <michele.caligiuri@paoloastori.com>
2026-06-14 16:33:59 +0200

    Testo formattato decentemente

--- info.php
+++ info.php
@@ -1,14 +1,25 @@
 <!-- Pagina delle info -->
 <div id="info">
-    <h3 title="Info">Info</h3>
-    <p>L'idea del glossario nacque da un progetto di scuola diretto dal professor Lorenzini. Lo scopo dell'attività era creare delle pagine
-    web in grado di mostrare all'utente determinate definizioni, inerenti a termini specifici su diversi argomenti che a loro volta appartengono a diverse
-    materie di scuola. L'attività era stata pensata per imparare ad utilizzare un database MYSQL con il linguaggio PHP e per ottenere un qualcosa
-    di utile per lo studente. Finimmo per lavorarci in 2, bel mondo la scuola vero?</p>
-    <p>Io mi occupavo della parte frontend, mentre il mio compare (Barber se ci sei batti un colpo) della probabilmente 
-    più complessa parte backend per un eventuale amministratore. 
-    Tutto sommato venne un buon lavoro, considerando il poco tempo a disposizione e le conoscenze di allora. </p>
-    <p>A oltre un anno di distanza e con qualche esperienza/conoscenza in più, ho deciso di riscrivere il glossario in 
-    entrambe le sue parti, sfruttando la tecnica AJAX. Quindi oltre ai classici HTML,CSS,PHP si è aggiunto anche un bel po' di JavaScript.
-    Per quanto riguarda il database è sostanzialmente rimasto lo stesso, poichè fu pensato con criterio e quindi non mi ha creato problemi riutilizzarlo.</p>
+  <h2 title="Info">Info</h2>
+  <p>
+    L'idea del glossario nacque nel 2016 da un progetto di scuola diretto dal professor <strong>Lorenzini</strong>. 
+    Lo scopo dell'attività era creare delle pagine web in grado di mostrare all'utente determinate definizioni, 
+    inerenti a termini specifici su diversi argomenti che a loro volta appartengono a diverse materie di scuola. 
+    L'attività era stata pensata per imparare ad utilizzare un database <strong>MySQL</strong> con il linguaggio <strong>PHP</strong> e per ottenere un qualcosa di utile per lo studente. 
+    Finimmo per lavorarci in 2, bel mondo la scuola vero?
+    Io mi occupavo della parte <strong>frontend</strong>, mentre il mio compare della probabilmente più complessa parte <strong>backend</strong> per un eventuale amministratore. 
+    Tutto sommato venne un buon lavoro, considerando il poco tempo a disposizione e le conoscenze di allora.
+  </p>
+
+  <p>
+    Nel 2019 con qualche esperienza/conoscenza in più, ho deciso di riscrivere il glossario in entrambe le sue parti, sfruttando la tecnica <strong>AJAX</strong>. 
+    Quindi oltre ai classici <strong>HTML, CSS, PHP</strong> si aggiunse anche un bel po' di <strong>JavaScript</strong>. 
+    Per quanto riguarda il database rimase lo stesso, poiché fu pensato con criterio e quindi non mi ha creato problemi riutilizzarlo.
+  </p>
+  
+  <p>
+    Nel 2026 dopo 7 anni di esperienza è stato eseguito un <strong>refactoring</strong> del codice tenendo conto delle moderne esigenze di sicurezza, cosa di cui il progetto originale, data la sua natura didattica, non poteva garantire. 
+    In questa versione si utilizza una sintassi più moderna di JavaScript e anche il database è stato rivisto in alcuni punti. 
+    Per maggiori dettagli consiglio di visitare il repository ufficiale su <a href="https://github.com/mcaligiuri/glossario" style="color: #0066cc; text-decoration: none; font-weight: bold;">GitHub</a>.
+  </p>
 </div>
